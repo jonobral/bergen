bcd100a48c665d5dc389a015d4e7ad5b87a6f290
Jonathan Obregón A <jonobral@gmail.com>
2016-08-22 09:50:24 -0600

    New webpack instance :v:

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'font-awesome/css/font-awesome.css'
+import './app.css';
+
+import {browserHistory, Router, Route} from 'react-router';
+
+const Home = React.createClass({
+	render: function() {
+		return (<div>Hello world</div>)
+	}
+});
+
+const routes = (
+	<Router>
+		<Route path="/" component={Home} />
+	</Router>
+);
+
+import App from 'containers/App/App';
+
+const mountNode = document.querySelector('#root');
+ReactDOM.render(
+	<App history={browserHistory} routes={routes} />, mountNode);
---
+++ src/containers/App/App.spec.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+
+import App from './App'
+import styles from './styles.module.css'
+
+describe('<App />', function () {
+	let wrapper;
+	beforeEach(() => {
+		wrapper = shallow(<App />)
+	});
+	
+	it('has a Router component', () => {
+		expect(wrapper.find('Router'))
+			.to.have.length(1);
+	});
+	
+	it('has a single wrapper element', () => {
+		expect(wrapper.find(`.${styles.wrapper}`))
+			.to.have.length(1);
+	});
+});
